a70efb5e14e463d5d3882e7d4a71a1aeaec58a90
Florin Opris <rev_oprisflorin@yahoo.com>
2019-03-11 21:02:36 +0200

    cleanup js intro

--- intro.js
+++ intro.js
@@ -1,17 +1,4 @@
-console.info("my first js file");
 
-var name = "Florin";
-var employed = true;
-
-console.warn("employed");
-console.warn(employed);
-
-console.info("Name:");
-console.info(name);
-
-name = "Florin";
-console.info("Name:");
-console.info(name);
 
 function getWelcomeMsg() {
     console.log("inside wellcome message");
@@ -37,24 +24,6 @@ console.info("summaryElement", summaryElement);
 var color = getColor();
 summaryElement.style.color = color;
 
-
-//first example 
-function hideHomePage() {
-    var el = document.getElementById('home-page');
-    el.style.display = 'none';
-}
-function showSkillsPage() {
-    document.getElementById('skills-page').style.display = 'block';
-}
-function clickOnSkills() {
-    //hideHomePage();
-    //showSkillsPage();
-    hidePage('home-page');
-    hidePage('languages-page');
-    hidePage('about-page');
-    showPage('skills-page');
-}
-//first example - end
 
 function hidePage(page) {
     var el = document.getElementById(page);
